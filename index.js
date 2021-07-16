c147febc82b1100a2b4013edcb680ce975c41344
lam-ignite <82447117+lam-ignite@users.noreply.github.com>
2021-07-16 23:50:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// javascript for index.html
+
+const renderPosts = async () => {
+  let uri = 'http://localhost:3000/posts';
+ 
+  
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
